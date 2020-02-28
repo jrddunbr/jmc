2a84c4aef662f8ead425de75008dd74d21be9641
Jared Dunbar <jrddunbr@gmail.com>
2020-02-27 20:53:38 -0500

    some more comments and such

--- useful.sh
+++ useful.sh
@@ -1,9 +1,16 @@
+# Don't use the following if you plan to use Home Manager
 nix-env -iA nixos.<pkgname>
-nixos-rebuild build
-nixos-rebuild switch
 
+# These are for the OS.
+nixos-rebuild build
+sudo nixos-rebuild switch
+
+# Edit them like this
+sudo vim /etc/nixos/configuration.nix
 
 # Home Manager stuff
 home-manager edit
 home-manager build
 home-manager switch
+
+# Protip - may want to add export EDITOR=vim before running home-manager edit
