51fe3e8e38e71afb49c46f2ac5cbba035ec0c9f6
Jared Dunbar <jrddunbr@gmail.com>
2020-02-27 20:34:58 -0500

    some home stuff

--- home.nix
+++ home.nix
@@ -3,7 +3,19 @@
 {
   home.packages = [
     pkgs.htop
+    #pkgs.discord
+    pkgs.dmidecode
+    pkgs.ffmpeg
+    pkgs.multimc
+    #pkgs.steam-run
+    pkgs.virt-manager
+    pkgs.vlc
+    pkgs.wireshark-qt
   ];
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+  };
 
   programs.firefox = {
     enable = true;
